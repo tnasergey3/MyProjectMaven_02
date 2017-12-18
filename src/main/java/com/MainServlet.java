d11145e60c1cb7ee89107eb9f0ea247a69a35806
tnasergey3 <tnasergey@gmail.com>
2017-12-18 05:27:35 +0200

    18.12.2017_02

--- src/main/java/com/MainServlet.java
+++ src/main/java/com/MainServlet.java
@@ -1,7 +1,9 @@
-//package src.main.re;
+package com;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
